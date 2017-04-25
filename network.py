cf94ceae152adcf96e2277340f680bdd00703ef4
Daniel Crispell <dan@visionsystemsinc.com>
2017-04-25 14:55:06 -0400

    replace transpose conv. with upsample + conv.
    This is an attempt to remove the "checkerboard" artifacts in the output
    images.

--- network.py
+++ network.py
@@ -15,14 +15,17 @@ class Unet(nn.Module):
         self.conv6 = nn.Conv2d(num_filters * 8, num_filters * 8, 4, 2, 1)
         self.conv7 = nn.Conv2d(num_filters * 8, num_filters * 8, 4, 2, 1)
         self.conv8 = nn.Conv2d(num_filters * 8, num_filters * 8, 4, 2, 1)
-        self.dconv1 = nn.ConvTranspose2d(num_filters * 8, num_filters * 8, 4, 2, 1)
-        self.dconv2 = nn.ConvTranspose2d(num_filters * 8 * 2, num_filters * 8, 4, 2, 1)
-        self.dconv3 = nn.ConvTranspose2d(num_filters * 8 * 2, num_filters * 8, 4, 2, 1)
-        self.dconv4 = nn.ConvTranspose2d(num_filters * 8 * 2, num_filters * 8, 4, 2, 1)
-        self.dconv5 = nn.ConvTranspose2d(num_filters * 8 * 2, num_filters * 4, 4, 2, 1)
-        self.dconv6 = nn.ConvTranspose2d(num_filters * 4 * 2, num_filters * 2, 4, 2, 1)
-        self.dconv7 = nn.ConvTranspose2d(num_filters * 2 * 2, num_filters, 4, 2, 1)
-        self.dconv8 = nn.ConvTranspose2d(num_filters * 2, channels_out, 4, 2, 1)
+
+        self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+
+        self.dconv1 = nn.Conv2d(num_filters*8, num_filters*8, 3, 1, 1)
+        self.dconv2 = nn.Conv2d(num_filters*8*2, num_filters*8, 3, 1, 1)
+        self.dconv3 = nn.Conv2d(num_filters*8*2, num_filters*8, 3, 1, 1)
+        self.dconv4 = nn.Conv2d(num_filters * 8 * 2, num_filters * 8, 3, 1, 1)
+        self.dconv5 = nn.Conv2d(num_filters * 8 * 2, num_filters * 4, 3, 1, 1)
+        self.dconv6 = nn.Conv2d(num_filters * 4 * 2, num_filters * 2, 3, 1, 1)
+        self.dconv7 = nn.Conv2d(num_filters * 2 * 2, num_filters, 3, 1, 1)
+        self.dconv8 = nn.Conv2d(num_filters * 2, channels_out, 3, 1, 1)
 
         self.batch_norm = nn.BatchNorm2d(num_filters)
         self.batch_norm2 = nn.BatchNorm2d(num_filters * 2)
@@ -57,28 +60,28 @@ class Unet(nn.Module):
         # Decoder
         # Deconvolution layers:
         # state size is (num_filters x 8) x 1 x 1
-        d1_ = self.batch_norm8(self.dconv1(self.relu(e8)))
+        d1_ = self.batch_norm8(self.dconv1(self.up(self.relu(e8))))
         # state size is (num_filters x 8) x 2 x 2
         d1 = torch.cat((d1_, e7), 1)
-        d2_ = self.batch_norm8(self.dconv2(self.relu(d1)))
+        d2_ = self.batch_norm8(self.dconv2(self.up(self.relu(d1))))
         # state size is (num_filters x 8) x 4 x 4
         d2 = torch.cat((d2_, e6), 1)
-        d3_ = self.batch_norm8(self.dconv3(self.relu(d2)))
+        d3_ = self.batch_norm8(self.dconv3(self.up(self.relu(d2))))
         # state size is (num_filters x 8) x 8 x 8
         d3 = torch.cat((d3_, e5), 1)
-        d4_ = self.batch_norm8(self.dconv4(self.relu(d3)))
+        d4_ = self.batch_norm8(self.dconv4(self.up(self.relu(d3))))
         # state size is (num_filters x 8) x 16 x 16
         d4 = torch.cat((d4_, e4), 1)
-        d5_ = self.batch_norm4(self.dconv5(self.relu(d4)))
+        d5_ = self.batch_norm4(self.dconv5(self.up(self.relu(d4))))
         # state size is (num_filters x 4) x 32 x 32
         d5 = torch.cat((d5_, e3), 1)
-        d6_ = self.batch_norm2(self.dconv6(self.relu(d5)))
+        d6_ = self.batch_norm2(self.dconv6(self.up(self.relu(d5))))
         # state size is (num_filters x 2) x 64 x 64
         d6 = torch.cat((d6_, e2), 1)
-        d7_ = self.batch_norm(self.dconv7(self.relu(d6)))
+        d7_ = self.batch_norm(self.dconv7(self.up(self.relu(d6))))
         # state size is (num_filters) x 128 x 128
         d7 = torch.cat((d7_, e1), 1)
-        d8 = self.dconv8(self.relu(d7))
+        d8 = self.dconv8(self.up(self.relu(d7)))
         # state size is (nc) x 256 x 256
         output = self.tanh(d8)
         return output
