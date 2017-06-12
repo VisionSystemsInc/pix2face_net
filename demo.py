8ca28ed3a681923375b29fc023e63866973d01f8
Daniel Crispell <dan@visionsystemsinc.com>
2017-06-11 22:35:23 -0400

    add demo script and test image

---
+++ demo.py
@@ -0,0 +1,31 @@
+""" pix2face demo:
+    loads an image, estimates PNCC + offset images, and outputs a 3D point cloud
+"""
+import pix2face
+import numpy as np
+from PIL import Image
+import os.path
+
+#----- parameters -----
+model_fname = './data/models/pix2face_unet_cuda75.pt'
+img_fname = './data/CASIA_0000107_004.jpg'
+output_dir = '.'
+#----------------------
+
+# Load the network weights
+model = pix2face.test.load_model(model_fname)
+
+# load the test image
+img = np.array(Image.open(img_fname))
+
+# run the image through te network
+outputs = pix2face.test.test(model, [img,])
+pncc = outputs[0][0]
+offsets = outputs[0][1]
+
+# PNCC + offsets = 3D point cloud
+# The ply file can be loaded with a 3D viewer such as meshlab
+basename = os.path.splitext(os.path.basename(img_fname))[0] 
+output_point_cloud_fname = os.path.join(output_dir, basename + '.ply')
+print('Writing ' + output_point_cloud_fname)
+pix2face.data.save_ply(img, pncc, offsets, output_point_cloud_fname)
