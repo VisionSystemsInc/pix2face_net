c88a0687a778e0b900196f0aadfc94075d98c670
Daniel Crispell <dan@visionsystemsinc.com>
2018-01-09 17:10:07 -0500

    dont share batch norm parameters across layers

--- pix2face/network.py
+++ pix2face/network.py
@@ -27,10 +27,19 @@ class Unet(nn.Module):
         self.dconv7 = nn.Conv2d(num_filters * 2 * 2, num_filters, 3, 1, 1)
         self.dconv8 = nn.Conv2d(num_filters * 2, channels_out, 3, 1, 1)
 
-        self.batch_norm = nn.BatchNorm2d(num_filters)
-        self.batch_norm2 = nn.BatchNorm2d(num_filters * 2)
-        self.batch_norm4 = nn.BatchNorm2d(num_filters * 4)
-        self.batch_norm8 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm2_dn = nn.BatchNorm2d(num_filters * 2)
+        self.batch_norm4_dn = nn.BatchNorm2d(num_filters * 4)
+        self.batch_norm8_dn1 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_dn2 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_dn3 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_dn4 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_up1 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_up2 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_up3 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm8_up4 = nn.BatchNorm2d(num_filters * 8)
+        self.batch_norm4_up = nn.BatchNorm2d(num_filters * 4)
+        self.batch_norm2_up = nn.BatchNorm2d(num_filters * 2)
+        self.batch_norm1_up = nn.BatchNorm2d(num_filters)
 
         self.leaky_relu = nn.LeakyReLU(0.2, True)
         self.relu = nn.ReLU(True)
@@ -42,17 +51,17 @@ class Unet(nn.Module):
         # input is (nc) x 256 x 256
         e1 = self.conv1(input)
         # state size is (num_filters) x 128 x 128
-        e2 = self.batch_norm2(self.conv2(self.leaky_relu(e1)))
+        e2 = self.batch_norm2_dn(self.conv2(self.leaky_relu(e1)))
         # state size is (num_filters x 2) x 64 x 64
-        e3 = self.batch_norm4(self.conv3(self.leaky_relu(e2)))
+        e3 = self.batch_norm4_dn(self.conv3(self.leaky_relu(e2)))
         # state size is (num_filters x 4) x 32 x 32
-        e4 = self.batch_norm8(self.conv4(self.leaky_relu(e3)))
+        e4 = self.batch_norm8_dn1(self.conv4(self.leaky_relu(e3)))
         # state size is (num_filters x 8) x 16 x 16
-        e5 = self.batch_norm8(self.conv5(self.leaky_relu(e4)))
+        e5 = self.batch_norm8_dn2(self.conv5(self.leaky_relu(e4)))
         # state size is (num_filters x 8) x 8 x 8
-        e6 = self.batch_norm8(self.conv6(self.leaky_relu(e5)))
+        e6 = self.batch_norm8_dn3(self.conv6(self.leaky_relu(e5)))
         # state size is (num_filters x 8) x 4 x 4
-        e7 = self.batch_norm8(self.conv7(self.leaky_relu(e6)))
+        e7 = self.batch_norm8_dn4(self.conv7(self.leaky_relu(e6)))
         # state size is (num_filters x 8) x 2 x 2
         # No batch norm on output of Encoder
         e8 = self.conv8(self.leaky_relu(e7))
@@ -60,25 +69,25 @@ class Unet(nn.Module):
         # Decoder
         # Deconvolution layers:
         # state size is (num_filters x 8) x 1 x 1
-        d1_ = self.batch_norm8(self.dconv1(self.up(self.relu(e8))))
+        d1_ = self.batch_norm8_up1(self.dconv1(self.up(self.relu(e8))))
         # state size is (num_filters x 8) x 2 x 2
         d1 = torch.cat((d1_, e7), 1)
-        d2_ = self.batch_norm8(self.dconv2(self.up(self.relu(d1))))
+        d2_ = self.batch_norm8_up2(self.dconv2(self.up(self.relu(d1))))
         # state size is (num_filters x 8) x 4 x 4
         d2 = torch.cat((d2_, e6), 1)
-        d3_ = self.batch_norm8(self.dconv3(self.up(self.relu(d2))))
+        d3_ = self.batch_norm8_up3(self.dconv3(self.up(self.relu(d2))))
         # state size is (num_filters x 8) x 8 x 8
         d3 = torch.cat((d3_, e5), 1)
-        d4_ = self.batch_norm8(self.dconv4(self.up(self.relu(d3))))
+        d4_ = self.batch_norm8_up4(self.dconv4(self.up(self.relu(d3))))
         # state size is (num_filters x 8) x 16 x 16
         d4 = torch.cat((d4_, e4), 1)
-        d5_ = self.batch_norm4(self.dconv5(self.up(self.relu(d4))))
+        d5_ = self.batch_norm4_up(self.dconv5(self.up(self.relu(d4))))
         # state size is (num_filters x 4) x 32 x 32
         d5 = torch.cat((d5_, e3), 1)
-        d6_ = self.batch_norm2(self.dconv6(self.up(self.relu(d5))))
+        d6_ = self.batch_norm2_up(self.dconv6(self.up(self.relu(d5))))
         # state size is (num_filters x 2) x 64 x 64
         d6 = torch.cat((d6_, e2), 1)
-        d7_ = self.batch_norm(self.dconv7(self.up(self.relu(d6))))
+        d7_ = self.batch_norm1_up(self.dconv7(self.up(self.relu(d6))))
         # state size is (num_filters) x 128 x 128
         d7 = torch.cat((d7_, e1), 1)
         d8 = self.dconv8(self.up(self.relu(d7)))
