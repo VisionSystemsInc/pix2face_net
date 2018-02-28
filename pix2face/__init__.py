feb0b2029d5fbfcdb90936fbe8958062f9f1ca4c
Daniel Crispell <dan@visionsystemsinc.com>
2018-02-28 13:30:50 -0500

    more relative import fixes

--- pix2face/__init__.py
+++ pix2face/__init__.py
@@ -1,5 +1,5 @@
-import network
-import data
-import train
-import test
+from . import network
+from . import data
+from . import train
+from . import test
 __all__ = ['network', 'data', 'train', 'test']
